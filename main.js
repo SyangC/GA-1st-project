fb4f3b71bdc6474ccce0b1c1aedd52fc2c420c75
Shu-yang Chia <shuyangchia@hotmail.co.uk>
2016-07-08 14:35:58 +0100

    able to move pcs individually, with basic information attached. The move functionality has also been coded so that the pcs will not be able to move after their movement has been used up.

--- main.js
+++ main.js
@@ -2,35 +2,57 @@ console.log("js has loaded")
 
 $ (function() {
 
-// on click of button
-  // $(".original").on("click", function() {
-  //   console.log("button has been clicked")
-  //   $("li").toggleClass('grid');
-  //   $("li").toggleClass('shu');
-  // });
+  var selectedCharacter = "";
+  // create player consturction function
+    function Player (name, divClass, healthTotal, health, attack, defense, movement, movementMax){
+      this.name = name;
+      this.divClass = divClass;
+      this.healthTotal = healthTotal;
+      this.health = health;
+      this.attack = attack;
+      this.defense = defense;
+      this.movement = movement;
+      this.movementMax = movementMax;
+    }
 
-// this is movement of div based on keypresses
-  // var $animation = $('#animation');
-  //   $(document).on("keydown", function(e) {
-  //     switch(e.key) {
-  //       case "a":
-  //       case "ArrowLeft":
-  //         $animation.css("left", "-=100px");
-  //         break;
-  //       case "w":
-  //       case "ArrowUp":
-  //         $animation.css("top", "-=102px");
-  //         break;
-  //       case "d":
-  //       case "ArrowRight":
-  //         $animation.css("left", "+=100px");
-  //         break;
-  //       case "s"key: "value", 
-  //       case "ArrowDown":
-  //         $animation.css("top", "+=102px");
-  //         break;
-  //     }
-  //   });
+
+
+// actual players
+    var pOneCha1 = new Player("Shu", "shu", 100, 100, 15, 15, 5, 5);
+
+    $("ul").on("click", "li.shu", function() {
+      $('#name').html(pOneCha1.name)
+      $('#health').html(pOneCha1.health + " / " + pOneCha1.healthTotal)
+      $('#attack').html(pOneCha1.attack)
+      $('#defense').html(pOneCha1.defense)
+      $('#movement').html(pOneCha1.movement + " / " + pOneCha1.movementMax)
+      selectedCharacter = pOneCha1;
+      console.log(selectedCharacter);
+    });
+
+    var pOneCha2 = new Player("Alpha", "alpha", 70, 70, 30, 5, 8, 8);
+
+    $("ul").on("click", "li.alpha", function() {
+      $('#name').html(pOneCha2.name)
+      $('#health').html(pOneCha2.health + " / " + pOneCha2.healthTotal)
+      $('#attack').html(pOneCha2.attack)
+      $('#defense').html(pOneCha2.defense)
+      $('#movement').html(pOneCha2.movement + " / " + pOneCha2.movementMax)
+      selectedCharacter = pOneCha2;
+      console.log(selectedCharacter);
+    });
+
+    var pTwoCha1 = new Player("Beta", "beta", 150, 150, 10, 40, 2, 2);
+
+    $("ul").on("click", "li.beta", function() {
+      $('#name').html(pTwoCha1.name)
+      $('#health').html(pTwoCha1.health + " / " + pTwoCha1.healthTotal)
+      $('#attack').html(pTwoCha1.attack)
+      $('#defense').html(pTwoCha1.defense)
+      $('#movement').html(pTwoCha1.movement + " / " + pTwoCha1.movementMax)
+      selectedCharacter = pTwoCha1;
+      console.log(selectedCharacter);
+    });
 
 
   // attempt 2 (woop! this works!)
@@ -39,57 +61,68 @@ $ (function() {
     switch(e.key) {
       case "a":
       case "ArrowLeft":
-        if ($(".shu").attr('id') === "a1" ||
-          $(".shu").attr('id') === "b1" ||
-          $(".shu").attr('id') === "c1" ||
-          $(".shu").attr('id') === "d1" ||
-          $(".shu").attr('id') === "e1" ||
-          $(".shu").attr('id') === "f1" ||
-          $(".shu").attr('id') === "g1" ||
-          $(".shu").attr('id') === "h1" ||
-          $(".shu").prev().attr('class') !== "grid") {
+        if ($("."+selectedCharacter.divClass).attr('id') === "a1" ||
+          $("."+selectedCharacter.divClass).attr('id') === "b1" ||
+          $("."+selectedCharacter.divClass).attr('id') === "c1" ||
+          $("."+selectedCharacter.divClass).attr('id') === "d1" ||
+          $("."+selectedCharacter.divClass).attr('id') === "e1" ||
+          $("."+selectedCharacter.divClass).attr('id') === "f1" ||
+          $("."+selectedCharacter.divClass).attr('id') === "g1" ||
+          $("."+selectedCharacter.divClass).attr('id') === "h1" ||
+          $("."+selectedCharacter.divClass).prev().attr('class') !== "grid" || selectedCharacter.movement === 0) {
         } else {
-          var originalGrid = $(".shu").next().attr('class')
           $animation.css("left", "-=100px");
-          $(".shu").prev().attr("class", 'shuNew');
-          $(".shuNew").next().attr('class', 'grid');
-          $(".shuNew").attr('class', 'shu');
+          $("."+selectedCharacter.divClass).prev().attr("class", 'new');
+          $(".new").next().attr('class', 'grid');
+          $(".new").attr('class', selectedCharacter.divClass);
+          selectedCharacter.movement--; 
+          console.log(selectedCharacter.movement)
+          $('#movement').html(selectedCharacter.movement + " / " + selectedCharacter.movementMax)
         }
         break;
       case "w":
       case "ArrowUp":
-        if ($(".shu").prevAll().eq(7).attr('class') !== "grid") {
+        if ($("."+selectedCharacter.divClass).prevAll().eq(7).attr('class') !== "grid" || selectedCharacter.movement === 0) {
         } else {
           $animation.css("top", "-=102px");
-          $(".shu").prevAll().eq(7).attr('class', 'shu');
-          $(".shu").nextAll().eq(7).attr('class', 'grid');
+          $("."+selectedCharacter.divClass).prevAll().eq(7).attr('class', selectedCharacter.divClass);
+          $("."+selectedCharacter.divClass).nextAll().eq(7).attr('class', 'grid');
+          selectedCharacter.movement--; 
+          console.log(selectedCharacter.movement)
+          $('#movement').html(selectedCharacter.movement + " / " + selectedCharacter.movementMax)
         }
         break;
       case "d":
       case "ArrowRight":
-        if ($(".shu").attr('id') === "a8" ||
-          $(".shu").attr('id') === "b8" ||
-          $(".shu").attr('id') === "c8" ||
-          $(".shu").attr('id') === "d8" ||
-          $(".shu").attr('id') === "e8" ||
-          $(".shu").attr('id') === "f8" ||
-          $(".shu").attr('id') === "g8" ||
-          $(".shu").attr('id') === "h8" || 
-          $(".shu").next().attr('class') !== "grid") {
+        if ($("."+selectedCharacter.divClass).attr('id') === "a8" ||
+          $("."+selectedCharacter.divClass).attr('id') === "b8" ||
+          $("."+selectedCharacter.divClass).attr('id') === "c8" ||
+          $("."+selectedCharacter.divClass).attr('id') === "d8" ||
+          $("."+selectedCharacter.divClass).attr('id') === "e8" ||
+          $("."+selectedCharacter.divClass).attr('id') === "f8" ||
+          $("."+selectedCharacter.divClass).attr('id') === "g8" ||
+          $("."+selectedCharacter.divClass).attr('id') === "h8" || 
+          $("."+selectedCharacter.divClass).next().attr('class') !== "grid" || selectedCharacter.movement === 0) {
         } else {
           $animation.css("left", "+=100px");
-          $(".shu").next().attr('class', 'shuNew');
-          $(".shuNew").prev().attr('class', 'grid');
-          $(".shuNew").attr('class', 'shu');
+          $("."+selectedCharacter.divClass).next().attr('class', 'new');
+          $(".new").prev().attr('class', 'grid');
+          $(".new").attr('class', selectedCharacter.divClass);
+          selectedCharacter.movement--; 
+          console.log(selectedCharacter.movement)
+          $('#movement').html(selectedCharacter.movement + " / " + selectedCharacter.movementMax)
         }
         break;
       case "s":
       case "ArrowDown":
-        if ($(".shu").nextAll().eq(7).attr('class') !== "grid") {
+        if ($("."+selectedCharacter.divClass).nextAll().eq(7).attr('class') !== "grid" || selectedCharacter.movement === 0) {
         } else {
           $animation.css("top", "+=102px");
-          $(".shu").nextAll().eq(7).attr('class', 'shu');
-          $(".shu").prevAll().eq(7).attr('class', 'grid');
+          $("."+selectedCharacter.divClass).nextAll().eq(7).attr('class', selectedCharacter.divClass);
+          $("."+selectedCharacter.divClass).prevAll().eq(7).attr('class', 'grid');
+          selectedCharacter.movement--; 
+          console.log(selectedCharacter.movement)
+          $('#movement').html(selectedCharacter.movement + " / " + selectedCharacter.movementMax)
         }
         break;
     }
@@ -97,13 +130,13 @@ $ (function() {
 
 
 
-// create player consturction function
-  // var Player = function (name, divClass){
-  //   this.name = name;
-  //   this.divClass = divClass;
-  // }
 
-// var playerCharacter = new Player("Shu", "shu")
+
+
+
+// attack
+  // pOneCha3.health = pOneCha3.health-15;
+
 
 
 
